725c6fc17a1b767afbd5f003b65e801510fb9fa8
Kabir Goel <kabirgoel.kg@gmail.com>
2022-06-28 00:16:34 -0700

    Parser WIP

--- src/parser.ts
+++ src/parser.ts
@@ -1,3 +1,5 @@
+import { lex, LexemeType } from "./lexer.ts";
+
 type Expression = number | string | boolean | null;
 type SExpression = Expression | SExpression[];
 
@@ -6,12 +8,30 @@ type SExpression = Expression | SExpression[];
  * @param blob The blob to parse.
  * @returns The S-Expression.
  * @throws Throws an error if the blob is not a valid S-Expression.
- * @example
- * ```
- * parse("(a b c)")
- * // => ["a", "b", "c"]
- * ```
  */
-export function parse(_blob: string): SExpression {
-  throw new Error("not implemented");
+export function parse(blob: string): SExpression {
+  const lexemes = lex(blob);
+  const ast: SExpression = [];
+  const location: number[] = []; // Stack of positions in the AST.
+  for (const lxm of lexemes) {
+    switch (lxm.type) {
+      case LexemeType.LParen:
+        location.push(ast.length);
+        ast.push([]);
+        break;
+      case LexemeType.RParen: {
+        if (location.length === 0) {
+          throw new Error("Unexpected ')'");
+        }
+        location.pop()!;
+        break;
+      }
+      case LexemeType.EOF:
+        break;
+      default:
+        throw new Error("not implemented");
+    }
+  }
+
+  return ast;
 }
