39c648bdc8f032858f61c5ebd675f26976f4ab0e
Kabir Goel <kabirgoel.kg@gmail.com>
2022-06-30 01:05:41 -0700

    add special forms to parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,10 +1,35 @@
-import { lex, Lexeme, LexemeType } from "./lexer.ts";
+import { Lexeme, LexemeType } from "./lexer.ts";
 import { node, Node, NodeType } from "./node.ts";
 
 function isAtom(lexeme: Lexeme) {
-  return [LexemeType.Identifier, LexemeType.Number, LexemeType.String].includes(
-    lexeme.type
-  );
+  return [
+    LexemeType.Identifier,
+    LexemeType.Number,
+    LexemeType.String,
+    LexemeType.HTMLComment,
+  ].includes(lexeme.type);
+}
+
+function isSpecial(lexeme: Lexeme): boolean {
+  return [
+    LexemeType.Backtick,
+    LexemeType.Comma,
+    LexemeType.SingleQuote,
+  ].includes(lexeme.type);
+}
+
+type SpecialForm = "quasiquote" | "quote" | "unquote";
+
+function toSpecialForm(lexeme: Lexeme): SpecialForm {
+  switch (lexeme.type) {
+    case LexemeType.Backtick:
+      return "quasiquote";
+    case LexemeType.Comma:
+      return "unquote";
+    case LexemeType.SingleQuote:
+      return "quote";
+  }
+  throw new Error("unimplemented special form");
 }
 
 function toAtom(lexeme: Lexeme): Node {
@@ -16,7 +41,7 @@ function toAtom(lexeme: Lexeme): Node {
     case LexemeType.String:
       return node(
         NodeType.String,
-        lexeme.value?.slice(1, lexeme.value.length - 1) || null
+        lexeme.value?.slice(1, lexeme.value.length - 1)!
       );
   }
   throw new Error("passed lexeme is not atomic");
@@ -25,23 +50,66 @@ function toAtom(lexeme: Lexeme): Node {
 export function parse(lexemes: Lexeme[]): Node {
   const stack: Node[] = [node(NodeType.Program, null, [])];
   while (true) {
-    const [lexeme, ...rest] = lexemes;
-    lexemes = rest;
+    const lexeme = lexemes[0];
+    if (lexeme.type === LexemeType.EOF) {
+      break;
+    }
+
+    lexemes.shift();
+
     if (isAtom(lexeme)) {
       stack[stack.length - 1].children.push(toAtom(lexeme));
-    } else if (lexeme.type === LexemeType.LParen) {
+      continue;
+    }
+    if (lexeme.type === LexemeType.LParen) {
       // Create a new list.
       const listNode = node(NodeType.List, null, []);
       stack[stack.length - 1].children.push(listNode);
       stack.push(listNode);
-    } else if (lexeme.type === LexemeType.RParen) {
+      continue;
+    }
+    if (lexeme.type === LexemeType.RParen) {
+      // Pop the stack.
       if (stack.length <= 1) {
         throw new Error("unmatched )");
       }
       stack.pop();
-    } else if (lexeme.type === LexemeType.EOF) {
-      break;
+      continue;
     }
+    if (isSpecial(lexeme)) {
+      const specialFormNode = node(
+        NodeType.Identifier,
+        toSpecialForm(lexeme),
+        []
+      );
+      // Need to peek ahead by one lexeme. We know there's an EOF pending, so
+      // it's safe to assume lexemes has at least one element.
+      const next = lexemes[0];
+      if (isAtom(next)) {
+        const listNode = node(NodeType.List, null, [
+          specialFormNode,
+          toAtom(lexeme),
+        ]);
+        stack[stack.length - 1].children.push(listNode);
+      } else if (next.type === LexemeType.LParen) {
+        const listNode = node(NodeType.List, null, [specialFormNode]);
+        stack[stack.length - 1].children.push(listNode);
+        stack.push(listNode);
+      } else {
+        throw new Error(`${lexeme.value} at illegal position`);
+      }
+      // Eat up the next token, since we've already taken care of it.
+      lexemes.shift();
+      continue;
+    }
+    if (lexeme.type === LexemeType.Comment) {
+      // Do nothing.
+      continue;
+    }
+    throw new Error(`unexpected ${lexeme.type} ${lexeme.value}`);
+  }
+  if (stack.length !== 1) {
+    throw new Error("unmatched (");
   }
   return stack[0];
 }
